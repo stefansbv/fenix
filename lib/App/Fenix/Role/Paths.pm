c2d0106d56ddfad6d2fb88e745ef865d53515e6c
Stefan Suciu <stefbv70@gmail.com>
2020-11-23 23:16:26 +0200

    Move the POD before the subs

--- lib/App/Fenix/Role/Paths.pm
+++ lib/App/Fenix/Role/Paths.pm
@@ -16,6 +16,15 @@ use App::Fenix::Types qw(
 );
 use namespace::autoclean;
 
+=head3 module_path
+
+Returns a relative path to the main module of a Fenix application.
+distribution.
+
+    L<Fenix-Name/lib/Fenix/Tk/App>
+
+=cut
+
 has 'module_path' => (
     is      => 'ro',
     isa     => Path,
@@ -23,6 +32,25 @@ has 'module_path' => (
         return path( qw(lib App Fenix Tk App) );
     },
 );
+
+=head3 module_path_exists
+
+Return true if C<module_path> exists.
+
+=cut
+
+sub module_path_exists {
+    my $self = shift;
+    my $path = $self->module_path;
+    return 1 if $path->is_dir;
+    return;
+}
+
+=head3 screen_module_path
+
+Return the relative path to the screen modules of a Fenix application.
+
+=cut
 
 has 'screen_module_path' => (
     is      => 'ro',
@@ -33,6 +61,14 @@ has 'screen_module_path' => (
         return path( $self->module_path, $self->module );
     },
 );
+
+=head3 sharedir
+
+Returns the relative path to the configurations of Fenix applications.
+
+    L<Fenix-Name/share/apps>
+
+=cut
 
 has 'sharedir' => (
     is       => 'ro',
@@ -53,6 +89,16 @@ has 'sharedir' => (
     },
 );
 
+=head3 configdir
+
+Returns the path to the user configurations.
+
+For example:
+
+    L</home/me/.local/share/.fenix/apps>
+
+=cut
+
 has 'configdir' => (
     is      => 'ro',
     isa     => Path,
@@ -69,12 +115,16 @@ has 'configdir' => (
     },
 );
 
-sub module_path_exists {
-    my $self = shift;
-    my $path = $self->module_path;
-    return 1 if $path->is_dir;
-    return;
-}
+=head3 app_path_for
+
+Return the application specific configurations path a specific
+subdirectory in L<configdir>, provided as a parameter.
+
+For example with L<etc> as parameter:
+
+    L</home/me/.local/share/.fenix/etc>
+
+=cut
 
 sub app_path_for {
     my ($self, $dir, $mnemo) = @_;
@@ -85,6 +135,30 @@ sub app_path_for {
     return;
 }
 
+=head3 exists_app_path_for
+
+Returns true if the user configurations path exists.
+
+=cut
+
+sub exists_app_path_for {
+    my ($self, $dir) = @_;
+    my $path = $self->app_path_for($dir);
+    return $path if $path->is_dir;
+    return;
+}
+
+=head3 framework_path_for
+
+Return the framework (Fenix) configurations path for a specific
+subdirectory in L<configdir>, provided as a parameter.
+
+For example with L<etc> as parameter:
+
+    L</home/me/.local/share/.fenix/apps/etc>
+
+=cut
+
 sub framework_path_for {
     my ( $self, $dir ) = @_;
     die "No parameter provided for 'framework_path_for'" unless $dir;
@@ -93,17 +167,22 @@ sub framework_path_for {
     return;
 }
 
-sub exists_app_path_for {
-    my ($self, $dir) = @_;
-    return 1 if $self->app_path_for($dir)->is_dir;
-    return;
-}
+=head3 exists_framework_path_for
+
+Returns true if the framework (Fenix) configurations path exists.
+
+=cut
 
 sub exists_framework_path_for {
     my ($self, $dir) = @_;
-    return 1 if $self->framework_path_for($dir)->is_dir;
+    my $path = $self->framework_path_for($dir);
+    return $path if $path->is_dir;
     return;
 }
+
+=head3 dist_path_for
+
+=cut
 
 sub dist_path_for {
     my ($self, $dir, $mnemo) = @_;
@@ -115,6 +194,10 @@ sub dist_path_for {
     return;
 }
 
+=head3 dist_sys_path_for
+
+=cut
+
 sub dist_sys_path_for {
     my ($self, $dir) = @_;
     die "No parameter provided for 'dist_sys_path_for'" unless $dir;
@@ -124,15 +207,25 @@ sub dist_sys_path_for {
     return;
 }
 
+=head3 exists_dist_path_for
+
+=cut
+
 sub exists_dist_path_for {
     my ($self, $dir) = @_;
-    return 1 if $self->dist_path_for($dir)->is_dir;
+    my $path = $self->dist_path_for($dir);
+    return $path if $path->is_dir;
     return;
 }
 
+=head3 exists_dist_sys_path_for
+
+=cut
+
 sub exists_dist_sys_path_for {
     my ($self, $dir) = @_;
-    return 1 if $self->dist_sys_path_for($dir)->is_dir;
+    my $path = $self->dist_sys_path_for($dir);
+    return $path if $path->is_dir;
     return;
 }
 
@@ -147,81 +240,5 @@ __END__
 =head1 SYNOPSIS
 
 =head1 DESCRIPTION
-
-=head1 INTERFACE
-
-=head2 ATTRIBUTES
-
-=head3 mnemonic
-
-The name of the directory under L<sharedir>.  By default it is a
-lower case of the C<module> attribute.
-
-=head3 module_path
-
-Returns a relative path to the main module of a Fenix application.
-distribution.
-
-    L<Fenix-Name/lib/Fenix/Tk/App>
-
-=head3 screen_module_path
-
-Return the relative path to the screen modules of a Fenix application.
-
-=head3 sharedir
-
-Returns the relative path to the configurations of Fenix applications.
-
-    L<Fenix-Name/share/apps>
-
-=head3 configdir
-
-Returns the path to the user configurations.
-
-For example:
-
-    L</home/me/.local/share/.fenix/apps>
-
-=head2 INSTANCE METHODS
-
-=head3 module_path_exists
-
-Return true if C<module_path> exists.
-
-=head3 relative_path_to_dist
-
-Returns a path relative to the new dist.
-
-=head3 app_path_for
-
-Return the application specific configurations path a specific
-subdirectory in L<configdir>, provided as a parameter.
-
-For example with L<etc> as parameter:
-
-    L</home/me/.local/share/.fenix/etc>
-
-=head3 framework_path_for
-
-Return the framework (Fenix) configurations path for a specific
-subdirectory in L<configdir>, provided as a parameter.
-
-For example with L<etc> as parameter:
-
-    L</home/me/.local/share/.fenix/apps/etc>
-
-=head3 exists_app_path_for
-
-Returns true if the user configurations path exists.
-
-=head3 exists_framework_path_for
-
-=head3 dist_path_for
-
-=head3 dist_sys_path_for
-
-=head3 exists_dist_path_for
-
-=head3 exists_dist_sys_path_for
 
 =cut
