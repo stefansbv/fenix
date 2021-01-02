ef61a730a996c49aaf9ae81c58e79e05a3473a9f
Stefan Suciu <stefbv70@gmail.com>
2021-01-02 15:58:29 +0200

    Add record module - creates a panel on the rec tab

---
+++ lib/App/Fenix/Panel/Record.pm
@@ -0,0 +1,30 @@
+package App::Fenix::Panel::Record;
+
+# ABSTRACT: Panel - Record
+
+use utf8;
+use Moo;
+use App::Fenix::Types qw(
+    Str
+);
+use Tk;
+use Tk::widgets qw(LabFrame JComboBox Panel);
+
+with qw/App::Fenix::Role::Panel
+        App::Fenix::Role::Element/;
+
+has 'name' => (
+    is      => 'ro',
+    isa     => Str,
+    default => sub { 'Record' },
+);
+
+sub _build_panel {
+    my $self = shift;
+    my $f = $self->nb_frame->Frame->pack( -padx => 5, -pady => 20 );
+    return $f;
+}
+
+__PACKAGE__->meta->make_immutable;
+
+1;
